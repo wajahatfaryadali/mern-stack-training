ed8dcd763d5fb45475bf24f87a364f51f3ada624
wajahat ali <wajahatfaryadali@gmail.com>
2025-05-14 15:51:22 +0500

    basic frontend vite app added

--- day_1/backend/server.js
+++ day_1/backend/server.js
@@ -1,8 +1,27 @@
-const express = require("express");
+import express from "express"
 
 const app = express();
 
 const PORT = 3000
+
+const jokes = [
+    "Why don't programmers like nature? It has too many bugs.",
+    "Why did the function break up with the variable? It had too many arguments.",
+    "How do you comfort a JavaScript bug? You console it.",
+    "Why do Java developers wear glasses? Because they don't C#.",
+    "I would tell you a joke about recursion... but you'd have to hear it again."
+];
+
+
+app.get("/", (req, res) => {
+    res.send("hello!")
+})
+
+app.get("/jokes", (req, res) => {
+    res.json(jokes)
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`)
---
+++ day_1/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  
+
+  return (
+    <>
+      <h1>hello duck</h1>
+    </>
+  )
+}
+
+export default App
