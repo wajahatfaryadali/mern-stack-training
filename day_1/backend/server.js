4a5cfc5407149bf71c3cb8c436fec31e1d4edad0
wajahat ali <wajahatfaryadali@gmail.com>
2025-05-14 16:11:37 +0500

    axios installed

--- day_1/backend/server.js
+++ day_1/backend/server.js
@@ -5,12 +5,13 @@ const app = express();
 const PORT = 3000
 
 const jokes = [
-    "Why don't programmers like nature? It has too many bugs.",
-    "Why did the function break up with the variable? It had too many arguments.",
-    "How do you comfort a JavaScript bug? You console it.",
-    "Why do Java developers wear glasses? Because they don't C#.",
-    "I would tell you a joke about recursion... but you'd have to hear it again."
+    { id: 1, joke: "Why don't programmers like nature? It has too many bugs." },
+    { id: 2, joke: "Why did the function break up with the variable? It had too many arguments." },
+    { id: 3, joke: "How do you comfort a JavaScript bug? You console it." },
+    { id: 4, joke: "Why do Java developers wear glasses? Because they don't C#." },
+    { id: 5, joke: "I would tell you a joke about recursion... but you'd have to hear it again." }
 ];
+
 
 
 app.get("/", (req, res) => {
--- day_1/frontend/src/App.jsx
+++ day_1/frontend/src/App.jsx
@@ -1,12 +1,37 @@
+import { useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
 
-  
+  const [jokes, setJokes] = useState([]);
+
+  const getJokes = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/jokes');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  useEffect(() => {
+    getJokes()
+    return () => {
+      setJokes([])
+    }
+  }, [])
+
 
   return (
     <>
       <h1>hello duck</h1>
+      <h2>total jokes : {jokes.length}</h2>
+      {jokes.map(joke => <div key={joke.id}>
+        {joke.joke}
+      </div>)}
     </>
   )
 }
